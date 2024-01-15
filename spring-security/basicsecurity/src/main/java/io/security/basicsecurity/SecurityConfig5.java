519136e9b9619fc81d981100458489af139351a7
Hambak-note <dudghks5722@gmail.com>
2024-01-15 20:19:54 +0900

    feat: 인가 개념 및 필터 이해 : Authorization, FilterSecurityInterceptor 실습 코드

--- spring-security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig5.java
+++ spring-security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig5.java
@@ -1,19 +1,17 @@
 package io.security.basicsecurity;
 
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfig4 {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig5 {
 
 
     @Bean
@@ -22,14 +20,14 @@ public class SecurityConfig4 {
         http
                 .authorizeHttpRequests(auth -> {
                     auth
-                            .anyRequest().authenticated();
+                            .requestMatchers("/user").hasRole("USER")
+                            .anyRequest().permitAll();
                 });
 
         http
                 .formLogin(Customizer.withDefaults());
 
 
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 
         return http.build();
     }
