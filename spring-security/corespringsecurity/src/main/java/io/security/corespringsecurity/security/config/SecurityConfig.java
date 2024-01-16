f32f9b82de1287637887bf813d3bf50fb8b1c03c
Hambak-note <dudghks5722@gmail.com>
2024-01-16 14:16:13 +0900

    feat: successHandler를 만들어 secufiry filter chain에 추가

--- spring-security/corespringsecurity/src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
+++ spring-security/corespringsecurity/src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +31,9 @@ public class SecurityConfig {
 
     @Autowired
     private AuthenticationDetailsSource authenticationDetailsSource;
+
+    @Autowired
+    private AuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
 
 //    @Autowired
@@ -103,6 +107,7 @@ public class SecurityConfig {
                             .loginProcessingUrl("/login_proc")
                             .authenticationDetailsSource(authenticationDetailsSource)
                             .defaultSuccessUrl("/")
+                            .successHandler(customAuthenticationSuccessHandler)
                             .permitAll();
                 });
 
