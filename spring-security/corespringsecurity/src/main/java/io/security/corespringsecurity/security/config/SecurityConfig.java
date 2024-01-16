fa31bc2614b859880199fa152fb9f150653e63d2
Hambak-note <dudghks5722@gmail.com>
2024-01-16 10:51:39 +0900

    feat: SecurityConfig 작성
    - 3명의 InMemory 사용자를 추가하고 권한에 따른 접근 설정
    - 패스워드를 Encoding해서 저장할 수 있도록 작성

---
+++ spring-security/corespringsecurity/src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package io.security.corespringsecurity.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        String password = passwordEncoder().encode("1111");
+
+        UserDetails user = User.withUsername("user")
+                .password(password).roles("USER")
+                .build();
+
+        UserDetails manager = User.withUsername("manager")
+                .password(password).roles("MANAGER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(password).roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, manager, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(authRequest -> {
+                    authRequest
+                            .requestMatchers("/").permitAll()
+                            .requestMatchers("/mypage").hasRole("USER")
+                            .requestMatchers("/messages").hasRole("MANAGER")
+                            .requestMatchers("/config").hasRole("ADMIN")
+                            .anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
